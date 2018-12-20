0c54c4289116a11938fe3b041b92f0ac1d294b43
minamorise <minamorise@gmail.com>
2018-12-20 13:15:12 +0200

    Add files via upload

---
+++ test.c
@@ -0,0 +1,21 @@
+/* ************************************* */
+/* ************ test file *********************/
+/*             multi use                */
+/* ************************************* */
+#include <stdio.h>
+  int main(void) {
+    int c =1;
+
+    switch (c) {
+      int x=5;
+      case 1:
+      printf("done \n" );
+	   printf("%d \n",x );
+	  break ;
+	  case 2 : 
+	  printf("done \n" );
+	  
+    }
+
+    return 0;
+  }
